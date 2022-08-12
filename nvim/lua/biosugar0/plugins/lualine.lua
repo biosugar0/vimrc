d3e4599b729c7f6e467f2c09ff8f841d4fc92977
Yuto Kimura <biosugar0@gmail.com>
2022-08-12 12:13:15 +0900

    fmt

--- nvim/lua/biosugar0/plugins/lualine.lua
+++ nvim/lua/biosugar0/plugins/lualine.lua
@@ -1,48 +1,47 @@
 local M = {}
-M.options =    {
-		icons_enabled = true,
-		theme = "nightfly",
-		component_separators = { left = "|", right = "|" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {},
-		always_divide_middle = true,
-		colored = true,
-	}
-M.sections =    {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff" },
-		lualine_c = {
-			{
-				"filename",
-				path = 1,
-				file_status = true,
-				shorting_target = 40,
-				symbols = {
-					modified = " [+]",
-					readonly = " [RO]",
-					unnamed = "Untitled",
-				},
+M.options = {
+	icons_enabled = true,
+	theme = "nightfly",
+	component_separators = { left = "|", right = "|" },
+	section_separators = { left = "", right = "" },
+	disabled_filetypes = {},
+	always_divide_middle = true,
+	colored = true,
+}
+M.sections = {
+	lualine_a = { "mode" },
+	lualine_b = { "branch", "diff" },
+	lualine_c = {
+		{
+			"filename",
+			path = 1,
+			file_status = true,
+			shorting_target = 40,
+			symbols = {
+				modified = " [+]",
+				readonly = " [RO]",
+				unnamed = "Untitled",
 			},
 		},
-		lualine_x = { "filetype", "encoding" },
-		lualine_y = {
-			{
-				"diagnostics",
-				source = { "nvim-lsp" },
-			},
+	},
+	lualine_x = { "filetype", "encoding" },
+	lualine_y = {
+		{
+			"diagnostics",
+			source = { "nvim-lsp" },
 		},
-		lualine_z = { "location" },
-	}
+	},
+	lualine_z = { "location" },
+}
 
-
-M.inactive_sections =    {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	}
+M.inactive_sections = {
+	lualine_a = {},
+	lualine_b = {},
+	lualine_c = { "filename" },
+	lualine_x = { "location" },
+	lualine_y = {},
+	lualine_z = {},
+}
 M.tabline = {}
 M.extensions = {}
 
