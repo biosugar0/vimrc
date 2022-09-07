ede0baa60beca2b9e4f334bc6a6eda6b8e3eacbb
Yuto Kimura <biosugar0@gmail.com>
2022-09-07 14:53:47 +0900

    rm

--- nvim/lua/biosugar0/plugins/nvim-treesitter.lua
+++ nvim/lua/biosugar0/plugins/nvim-treesitter.lua
@@ -22,28 +22,4 @@ require("nvim-treesitter.configs").setup({
 	indent = {
 		enable = true,
 	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-				["ib"] = "@block.inner",
-				["ab"] = "@block.outer",
-				["c"] = "@comment.outer",
-				["if"] = "@function.inner",
-				["af"] = "@function.outer",
-				["il"] = "@loop.inner",
-				["al"] = "@loop.outer",
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<M-s>"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<M-S-s>"] = "@parameter.inner",
-			},
-		},
-	},
 })
