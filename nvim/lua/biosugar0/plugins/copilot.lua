da68fd29429a34bdfcdabb339b417562fe9a57a5
Yuto Kimura <biosugar0@gmail.com>
2022-09-11 00:16:30 +0900

    fmt

--- nvim/lua/biosugar0/plugins/copilot.lua
+++ nvim/lua/biosugar0/plugins/copilot.lua
@@ -2,9 +2,9 @@ vim.g.copilot_no_maps = true
 vim.g.copilot_hide_during_completion = false
 vim.g.copilot_node_command = "~/.volta/bin/node"
 vim.keymap.set("i", "<c-l>", [[copilot#Accept()]], {
-    expr = true,
-    script = true,
+	expr = true,
+	script = true,
 })
 
-vim.keymap.set('i', '<M-]>', [[<Cmd>call copilot#Next()<CR>]])
-vim.keymap.set('i', '<M-[>', [[<Cmd>call copilot#Previous()<CR>]])
+vim.keymap.set("i", "<M-]>", [[<Cmd>call copilot#Next()<CR>]])
+vim.keymap.set("i", "<M-[>", [[<Cmd>call copilot#Previous()<CR>]])
