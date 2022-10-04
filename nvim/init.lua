670dd97b450b029f5cb8daf46c3070aba6421cb3
Yuto Kimura <biosugar0@gmail.com>
2022-10-04 20:31:36 +0900

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,9 @@ vim.cmd("filetype plugin indent on")
 vim.opt.rtp:append("~/.cache/dein/repos/github.com/vim-denops/denops.vim")
 vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc.vim")
 vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/pum.vim")
-vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-around.vim")
-vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-sorter_rank.vim")
-vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-matcher_head.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-around")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-sorter_rank")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-matcher_head")
 
 local ddc = {
 	patch_global = vim.fn["ddc#custom#patch_global"],
@@ -39,3 +39,11 @@ ddc.patch_global("autoCompleteEvents", {
 ddc.patch_global("completionMenu", "pum.vim")
 
 vim.fn["ddc#enable"]()
+
+vim.keymap.set(
+	"i",
+	[[<Tab>]],
+	[[ddc#map#pum_visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' : ddc#manual_complete()]],
+	{ noremap = false, expr = true }
+)
+
