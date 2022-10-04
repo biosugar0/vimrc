718be759a795b4d4df3d568e87d13b68540e2dd8
Yuto Kimura <biosugar0@gmail.com>
2022-10-04 20:21:48 +0900

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,51 +1,41 @@
-vim.env.VIMHOME = vim.fn.expand("<sfile>:p:h")
-vim.g.rc_dir = vim.env.VIMHOME
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-local set = vim.opt
+vim.cmd("syntax enable")
+vim.cmd("filetype plugin indent on")
 
-if vim.fn.has("vim_starting") == 1 then
-	vim.o.packpath = ""
-	set.encoding = "utf-8"
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/vim-denops/denops.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/pum.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-around.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-sorter_rank.vim")
+vim.opt.rtp:append("~/.cache/dein/repos/github.com/Shougo/ddc-matcher_head.vim")
 
-	vim.g.colorterm = os.getenv("COLORTERM")
-	if vim.fn.exists("+termguicolors") == 1 then
-		vim.o.termguicolors = true
-	end
-	-- 必要ないデフォルトプラグインを読み込まない
-	vim.g.did_install_default_menus = 1
-	vim.g.did_install_syntax_menu = 1
-	vim.g.loaded_2html_plugin = 1
-	vim.g.loaded_getscript = 1
-	vim.g.loaded_getscriptPlugin = 1
-	vim.g.loaded_gzip = 1
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwFileHandlers = 1
-	vim.g.loaded_netrwPlugin = 1
-	vim.g.loaded_netrwSettings = 1
-	vim.g.loaded_rrhelper = 1
-	vim.g.loaded_tar = 1
-	vim.g.loaded_tarPlugin = 1
-	vim.g.loaded_vimball = 1
-	vim.g.loaded_vimballPlugin = 1
-	vim.g.loaded_zipPlugin = 1
-	vim.g.loaded_zip = 1
-	vim.g.no_gvimrc_example = 1
-	vim.g.no_vimrc_example = 1
-	vim.g.skip_loading_mswin = 1
-	vim.g.loaded_man = 1
-	vim.g.loaded_tutor_mode_plugin = 1
-	set.visualbell = false
-	vim.o.t_vb = ""
-	if vim.fn.exists("&belloff") then
-		set.belloff = "all"
-	end
-	set.timeout = true
-	set.ttimeout = true
-	set.ttimeoutlen = 100
-	set.updatetime = 800
-	vim.env.CACHE = vim.fn.expand("~/.cache")
-end
+local ddc = {
+	patch_global = vim.fn["ddc#custom#patch_global"],
+	patch_buffer = vim.fn["ddc#custom#patch_buffer"],
+	patch_filetype = vim.fn["ddc#custom#patch_filetype"],
+	set_buffer = vim.fn["ddc#custom#set_buffer"],
+	set_context = vim.fn["ddc#custom#set_context"],
+}
 
-require("biosugar0")
-set.secure = true
+ddc.patch_global("sources", { "around" })
+ddc.patch_global("sourceOptions", {
+	_ = {
+		ignoreCase = true,
+		matchers = { "matcher_head" },
+		sorters = { "sorter_rank" },
+	},
+	around = {
+		mark = "A",
+	},
+})
+
+ddc.patch_global("autoCompleteEvents", {
+	"InsertEnter",
+	"TextChangedI",
+	"TextChangedP",
+	"CmdlineEnter",
+	"CmdlineChanged",
+})
+
+ddc.patch_global("completionMenu", "pum.vim")
+
+vim.fn["ddc#enable"]()
