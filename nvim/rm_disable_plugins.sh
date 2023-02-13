9cf838d53285f6856efd55a8e0858bb65a4a7d55
Yuto Kimura <biosugar0@gmail.com>
2023-02-13 10:11:03 +0900

    update

--- nvim/rm_disable_plugins.sh
+++ nvim/rm_disable_plugins.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 # Remove default plugins
-if [ -d /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/ ]; then
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/gzip.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/health.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/man.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/man.lua;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/matchit.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/matchparen.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/netrwPlugin.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/shada.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/spellfile.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/tarPlugin.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/tohtml.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/tutor.vim;
-    rm /usr/local/Cellar/neovim/0.8.0/share/nvim/runtime/plugin/zipPlugin.vim;
+if [ -d /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/ ]; then
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/gzip.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/health.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/man.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/man.lua;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/matchit.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/matchparen.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/netrwPlugin.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/netrwPlugin.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/shada.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/spellfile.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tarPlugin.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tohtml.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tutor.vim;
+    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/zipPlugin.vim;
 fi
