ec72fe5ca11c795f1a8921eee61f1a570491fc40
Yuto Kimura <biosuagr0@gmail.com>
2023-02-23 19:21:32 +0900

    update

--- nvim/lua/biosugar0/plugins/smooth-cursor.lua
+++ nvim/lua/biosugar0/plugins/smooth-cursor.lua
@@ -1,7 +1,6 @@
 local conf = {
 	autostart = true,
 	cursor = "", -- cursor shape (need nerd font)
-	intervals = 35, -- tick interval
 	linehl = nil, -- highlight sub-cursor line like 'cursorline', "CursorLine" recommended
 	type = "default", -- define cursor movement calculate function, "default" or "exp" (exponential).
 	fancy = {
@@ -17,11 +16,12 @@ local conf = {
 		},
 		tail = { cursor = nil, texthl = "SmoothCursorAqua" },
 	},
+	speed = 25, -- max is 100 to stick to your current position
+	intervals = 35, -- tick interval
 	priority = 10, -- set marker priority
-	speed = 40, -- max is 100 to stick to your current position
-	texthl = "SmoothCursor", -- highlight group, default is { bg = nil, fg = "#FFD400" }
-	threshold = 3,
 	timeout = 3000,
+	threshold = 3,
+	texthl = "SmoothCursor", -- highlight group, default is { bg = nil, fg = "#FFD400" }
 }
 
 require("smoothcursor").setup(conf)
