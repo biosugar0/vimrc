fc363ab1eaed39487cefc12770dcaf991e636d66
Yuto Kimura <biosugar0@gmail.com>
2023-04-03 15:20:43 +0900

    add gina-commit to copilot

--- nvim/lua/biosugar0/plugins/copilot.lua
+++ nvim/lua/biosugar0/plugins/copilot.lua
@@ -12,10 +12,17 @@ require("copilot").setup({
 	filetypes = {
 		yaml = true,
 		markdown = true,
+		["gina-commit"] = true,
 		["."] = true,
 	},
 	copilot_node_command = "node", -- Node.js version must be > 16.x
 	server_opts_overrides = {},
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = "MyAutoCmd",
+	pattern = "*",
+	command = [[if &filetype == 'gina-commit' | setlocal buflisted | endif]],
 })
 
 require("copilot.auth").signin() -- Sign in to Copilot
