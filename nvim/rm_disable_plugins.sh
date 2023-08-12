580fc8a9c3df6fa8bebe5f8e4c55bf7302580f64
Yuto Kimura <biosuagr0@gmail.com>
2023-08-12 09:33:25 +0900

    for M1

--- nvim/rm_disable_plugins.sh
+++ nvim/rm_disable_plugins.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 
 # Remove default plugins
-if [ -d /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/ ]; then
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/gzip.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/health.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/man.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/man.lua;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/matchit.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/matchparen.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/netrwPlugin.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/netrwPlugin.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/shada.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/spellfile.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tarPlugin.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tohtml.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/tutor.vim;
-    rm /usr/local/Cellar/neovim/0.8.3/share/nvim/runtime/plugin/zipPlugin.vim;
+if [ -d /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/ ]; then
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/gzip.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/health.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/man.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/man.lua
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/matchit.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/matchparen.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/netrwPlugin.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/netrwPlugin.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/shada.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/spellfile.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tarPlugin.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tohtml.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tutor.vim
+	rm /usr/local/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/zipPlugin.vim
+fi
+
+# if M1 Mac, /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/
+if [ -d /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/ ]; then
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/gzip.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/health.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/man.lua
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/matchit.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/matchparen.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/netrwPlugin.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/shada.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/spellfile.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tarPlugin.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tohtml.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/tutor.vim
+	rm /opt/homebrew/Cellar/neovim/0.9.1/share/nvim/runtime/plugin/zipPlugin.vim
 fi
